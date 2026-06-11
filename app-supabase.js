3086f60eb82020806b615196fe9051b23e02d97f
Lerghik <106703976+Lerghik@users.noreply.github.com>
2026-06-10 21:05:07 -0300

    Update app-supabase.js

--- app-supabase.js
+++ app-supabase.js
@@ -1,6 +1,6 @@
 // CONFIGURACIÓN DE TU PROYECTO
 const SUPABASE_URL = "https://ebglmoumxipvrtpsgkrw.supabase.co"; 
-const SUPABASE_ANON_KEY = "TU_API_KEY_ANONIMA_LARGA..."; // <--- PONÉ ACÁ TU KEY REAL EN GITHUB
+const SUPABASE_ANON_KEY = "sb_publishable_rKZ87GIXN8TtKlOsdeYN2g__gocmwd0"; // <--- PONÉ ACÁ TU KEY REAL EN GITHUB
 
 const miSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
